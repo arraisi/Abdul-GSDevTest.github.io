061ba0ea0fe327848fbf067752979ce57d5b40ac
Abdul R Arraisi <arraisi@icloud.com>
2021-05-23 17:39:47 +0700

    first commit

---
+++ src/test/java/com/geekseat/abdul/technical/test/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekseat.abdul.technical.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
